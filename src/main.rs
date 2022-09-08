e05c09290274b82a4a3c4ceeb3ef4f3a2fac7b73
greg <greg3@tutamail.com>
2022-09-08 17:36:24 -0300

    Adding colors

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
 
 fn main() {
-    println!(" - Guess the number\n");
+    println!(" \x1b[34m-\x1b[0m Guess the number\n");
     let secret_number = thread_rng().gen_range(1..101);
 
     loop {
         let guess: String = Input::new()
-            .with_prompt(" ! Input your guess")
+            .with_prompt(" \x1b[32m!\x1b[0m Input your guess")
             .interact_text()
             .unwrap();
 
@@ -20,26 +20,26 @@ fn main() {
         let guess: isize = match guess.parse() {
             Ok(num) => {
                 if num < 1 {
-                    println!(" @ Please, Enter a number greater than 0\n");
+                    println!(" \x1b[31;1m@\x1b[0m Please, Enter a number greater than 0\n");
                     continue;
                 } else if num > 100 {
-                    println!(" @ Please, Enter a number less than 100\n");
+                    println!(" \x1b[31;1m@\x1b[0m Please, Enter a number less than 100\n");
                     continue;
                 } else {
                     num
                 }
             }
             Err(_) => {
-                println!(" @ Please, Enter a number\n");
+                println!(" \x1b[31;1m@\x1b[0m Please, Enter a valid number\n");
                 continue;
             }
         };
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!(" @ Too small!\n"),
-            Ordering::Greater => println!(" @ Too big!\n"),
+            Ordering::Less => println!(" \x1b[31m@\x1b[0m Too small!\n"),
+            Ordering::Greater => println!(" \x1b[31m@\x1b[0m Too big!\n"),
             Ordering::Equal => {
-                println!(" - You win!");
+                println!(" \x1b[34m-\x1b[0m You win!");
                 break;
             }
         }
