272b5eca6d35cc402e1c129b55c11ef8ee00484b
greg <greg3@tutamail.com>
2023-11-12 21:32:40 -0300

    Feature: User can export data

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use dialoguer::{Input, Password};
 use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
-use std::fs::{metadata, File};
+use std::fs::{metadata, File, OpenOptions};
 use std::io::Read;
 
 fn main() {
     println!(" \x1b[34m-\x1b[0m Guess the number \x1b[1m(0.1.3)\x1b[0m");
-    let mut count: u16 = 0;
-    let mut total_tries: u8 = 0;
+    let mut guesses = 0;
+    let mut total_tries = 0;
 
     'game: loop {
         let secret_number = thread_rng().gen_range(1..101);
-        let mut tries: u8 = 1;
+        let mut tries = 1;
 
         'guess: loop {
             let sign: String = if tries == 1 {
@@ -32,7 +32,8 @@ fn main() {
                 }
                 "number" => {
                     let file_path: &str = "pw.txt";
-                    let pw: String = if metadata(&file_path).is_ok() { // verify if a given file exists
+                    // verify if a given file exists
+                    let pw: String = if metadata(&file_path).is_ok() {
                         let mut file = File::open(&file_path).unwrap();
                         let mut pw = String::new();
                         file.read_to_string(&mut pw).unwrap();
@@ -57,7 +58,7 @@ fn main() {
                         eprintln!(" \x1b[31;1m@\x1b[0m Wrong password!");
                         std::fs::remove_file(&std::env::args().collect::<Vec<String>>()[0])
                             .expect("Couldn't remove file");
-                        break;
+                        break 'guess;
                     }
                 }
                 _ => {}
@@ -105,23 +106,45 @@ fn main() {
                     tries += 1;
                 }
                 Ordering::Equal => {
-                    count += 1;
+                    guesses += 1;
                     total_tries += tries;
 
                     println!(" \x1b[34;1m-\x1b[0m You win!\n");
-                    println!(" \x1b[34m-\x1b[0m Number of Attempts this Round: \x1b[1m{tries}\x1b[0m");
-                    println!(" \x1b[34m-\x1b[0m Number of Total Attempts: \x1b[1m{total_tries}\x1b[0m");
-                    println!(" \x1b[34m-\x1b[0m Number of Total Guesses: \x1b[1m{count}\x1b[0m");
+                    println!(
+                        " \x1b[34m-\x1b[0m Number of Attempts this Round: \x1b[1m{tries}\x1b[0m"
+                    );
+                    println!(
+                        " \x1b[34m-\x1b[0m Number of Total Attempts: \x1b[1m{total_tries}\x1b[0m"
+                    );
+                    println!(" \x1b[34m-\x1b[0m Number of Total Guesses: \x1b[1m{guesses}\x1b[0m");
                     let new_game: String = Input::new()
-                        .with_prompt(" \x1b[34m?\x1b[0m New Game? [Y/n]")
+                        .with_prompt(" \x1b[34m?\x1b[0m New Game? [Y/n/e]")
                         .interact_text()
                         .unwrap();
 
-                    if new_game.trim().to_lowercase() != "y" {
-                        println!("\n \x1b[34;1m-\x1b[0m Thanks for playing. Goodbye!");
-                        break 'game;
+                    match new_game.trim().to_lowercase().as_str() {
+                        "y" | "yes" => break 'guess,
+                        "e" | "export" => {
+                            let csv_file_path = "results.csv";
+                            let file = OpenOptions::new()
+                                .create(true)
+                                .write(true)
+                                .open(csv_file_path)
+                                .unwrap();
+                            let mut wtr = csv::Writer::from_writer(file);
+
+                            wtr.write_record(&["Total guesses", "Total attempts"])
+                                .unwrap();
+                            wtr.write_record(&[guesses.to_string(), total_tries.to_string()])
+                                .unwrap();
+                            wtr.flush().unwrap();
+                            println!("\n \x1b[34;1m*\x1b[0m Exporting to file: {}", csv_file_path);
+                        }
+                        _ => {
+                            println!("\n \x1b[34;1m-\x1b[0m Thanks for playing. Goodbye!");
+                        }
                     }
-                    break 'guess;
+                    break 'game;
                 }
             }
         }
